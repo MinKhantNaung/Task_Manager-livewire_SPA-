fd3f8f7fbee48e2ad49ae1ced4e1524238022078
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-02-13 21:13:27 +0630

    add validation in store task

--- app/Livewire/Tasks/TaskIndex.php
+++ app/Livewire/Tasks/TaskIndex.php
@@ -3,19 +3,33 @@
 namespace App\Livewire\Tasks;
 
 use App\Models\Task;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class TaskIndex extends Component
 {
+    #[Validate('required|min:5|max:30|string')]
     public $title;
+
+    #[Validate('required|min:5|max:30|string')]
     public $slug;
+
+    #[Validate('required|min:20|string')]
     public $description;
+
+    #[Validate('required')]
     public $status;
+
+    #[Validate('required')]
     public $priority;
+
+    #[Validate('required|date')]
     public $deadline;
 
     public function saveTask()
     {
+        $this->validate();
+
         Task::create([
             'user_id' => auth()->user()->id,
             'title' => $this->title,
