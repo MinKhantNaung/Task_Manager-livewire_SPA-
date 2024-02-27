e19cd6a4720692583584134a1a09c20e1d6430b2
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-02-27 17:27:26 +0630

    change count design

--- resources/views/livewire/tasks/tasks-count.blade.php
+++ resources/views/livewire/tasks/tasks-count.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <div class="text-white px-3 py-2 text-2xl flex justify-between">
+    <div class="px-3 py-2 text-2xl flex justify-between">
         @foreach ($tasksByStatus as $status)
-            <div class="text-center bg-fuchsia-500 p-2 rounded">
-                <p>{{ $status->count }}</p>
+            <div class="text-center p-2 rounded">
+                <p class="rounded-full border border-orange-700">{{ $status->count }}</p>
                 <p>{{ $status->status }}</p>
             </div>
         @endforeach
